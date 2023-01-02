bab5bfe4007edbe898fd8e759b9184def2396e0e
Arda Çimen <ardacmen@gmail.com>
2023-01-02 13:14:45 +0300

    ui updated

---
+++ YKS/YKS/View/MainPageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainPageViewController.swift
+//  YKS
+//
+//  Created by Arda Çimen on 2.01.2023.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
---
+++ YKS/YKS/ViewModel/MainPageViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MainPageViewModel.swift
+//  YKS
+//
+//  Created by Arda Çimen on 2.01.2023.
+//
+
+import Foundation
+
+final class MainPageViewModel
+{
+    
+}
